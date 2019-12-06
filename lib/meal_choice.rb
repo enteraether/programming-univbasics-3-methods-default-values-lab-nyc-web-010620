89d0bfd1c3c5b96997c15971b172ca22f4fbfc48
Theresa <thetheresagarcia@gmail.com>
2019-12-06 03:46:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,17 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(veg1, veg2, protein = 'meat')
-  puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-end
+# def meal_choice(veg1, veg2, protein = 'meat')
+#   puts "What a nutritious meal!"
+#   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+# end
+#
+# meal_choice(peas,carrots,fish)
 
-meal_choice(peas,carrots,fish)
+
+def meal_choice(veg1, veg2, protein='meat')
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal # return here is optional. Ruby methods automatically return the last expression
+end
